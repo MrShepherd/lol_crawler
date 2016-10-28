3c22bc6d87c2d65ef8281110e5c314f84e2c6e75
shepherd <heimd4ll@yeah.net>
2016-10-29 01:11:49 +0800

    v1.3

--- gameid_info_crawler.py
+++ gameid_info_crawler.py
@@ -1,5 +1,6 @@
-import random
+import os
 import time
+from multiprocessing.dummy import Pool
 from urllib import parse, request
 
 from selenium import webdriver
@@ -7,7 +8,6 @@ from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from multiprocessing.dummy import Pool
 
 import htmlparser
 
@@ -22,14 +22,15 @@ class GameIDInfoCrawler(object):
         self.failed_page_urls = []
         self.pages = []
         self.gameid_data = []
+        self.id_mapping = []
         self.img_path = 'img/gameid/'
 
     def page_generator(self, url):
+        self.failed_page_urls.remove(url)
         browser = webdriver.PhantomJS(executable_path='/opt/phantomjs/bin/phantomjs')
         # browser = webdriver.Chrome()
         browser.set_page_load_timeout(15)
         print('getting:', url)
-        self.failed_page_urls.remove(url)
         # get url
         try:
             browser.get(url)
@@ -38,11 +39,15 @@ class GameIDInfoCrawler(object):
         # click button
         try:
             browser.find_element_by_xpath('//div[@class="Buttons"]/button[contains(text(),"Check MMR")]').click()
-            WebDriverWait(browser, 60).until(EC.presence_of_element_located((By.ID, 'ExtraView')))
+            WebDriverWait(browser, 120).until(EC.presence_of_element_located((By.ID, 'ExtraView')))
         except NoSuchElementException:
             try:
+                try:
+                    browser.get(url)
+                except TimeoutException:
+                    browser.execute_script('window.stop()')
                 browser.find_element_by_xpath('//div[@class="Buttons"]/button[contains(text(),"Check MMR")]').click()
-                WebDriverWait(browser, 60).until(EC.presence_of_element_located((By.ID, 'ExtraView')))
+                WebDriverWait(browser, 120).until(EC.presence_of_element_located((By.ID, 'ExtraView')))
             except NoSuchElementException:
                 self.failed_page_urls.append(url)
                 browser.close()
@@ -56,11 +61,15 @@ class GameIDInfoCrawler(object):
         # click link
         try:
             browser.find_element_by_xpath('//div[@class="RealContent"]//li[@data-type="ranked"]/a').click()
-            WebDriverWait(browser, 60).until(EC.presence_of_element_located((By.ID, 'WinRatioSparkline')))
+            WebDriverWait(browser, 120).until(EC.presence_of_element_located((By.ID, 'WinRatioSparkline')))
         except NoSuchElementException:
             try:
+                try:
+                    browser.get(url)
+                except TimeoutException:
+                    browser.execute_script('window.stop()')
                 browser.find_element_by_xpath('//div[@class="RealContent"]//li[@data-type="ranked"]/a').click()
-                WebDriverWait(browser, 60).until(EC.presence_of_element_located((By.ID, 'WinRatioSparkline')))
+                WebDriverWait(browser, 120).until(EC.presence_of_element_located((By.ID, 'WinRatioSparkline')))
             except NoSuchElementException:
                 self.failed_page_urls.append(url)
                 browser.close()
@@ -85,26 +94,23 @@ class GameIDInfoCrawler(object):
         except TimeoutException:
             self.browser.execute_script('window.stop()')
         print('get %s successfully' % self.url)
-        count = 1
-        while count < 16:
-            print(count, end='===>')
-            js = "document.body.scrollTop=%d000" % (count * 500)
-            self.browser.execute_script(js)
-            time.sleep(2)
-            count += 1
-        time.sleep(10)
-        # page_ladder = self.browser.page_source
-        # print(len(page_ladder))
+        # count = 1
+        # while count < 16:
+        #     print(count, end='===>')
+        #     js = "document.body.scrollTop=%d000" % (count * 500)
+        #     self.browser.execute_script(js)
+        #     time.sleep(2)
+        #     count += 1
+        # time.sleep(10)
         all_player = self.browser.find_element_by_xpath('//tbody[@class="Body"]').find_elements_by_xpath('//tr[contains(@class,"Row")]')
         print('number of player found:', len(all_player))
         for player in all_player[1:-1]:
-            if player.find_elements_by_tag_name('td')[3].text == 'Diamond 1':
-                break
             if player.find_elements_by_tag_name('td')[3].text in ['Challenger', 'Master']:
                 tmp_link = player.find_element_by_tag_name('a').get_attribute('href')
                 tmp_full_link = parse.urljoin(self.url, tmp_link)
                 print('append:', tmp_full_link)
                 self.page_urls.append(tmp_full_link)
+                # break
         print('length of url appended:', len(self.page_urls))
         self.failed_page_urls = self.page_urls
         while len(self.failed_page_urls) != 0:
@@ -112,33 +118,50 @@ class GameIDInfoCrawler(object):
             pool.map(self.page_generator, self.failed_page_urls)
             pool.close()
             pool.join()
+        print('number of pages downloaded:', len(self.pages))
 
     def craw_gameid_info(self):
         base_url = 'http://www.op.gg/summoner/'
+        # self.pages.append(request.urlopen('http://www.op.gg/summoner/userName=Jin%20Air%20Winged'))
+        # self.pages.append(request.urlopen('http://www.op.gg/summoner/userName=%EA%B0%95%EC%B2%A0%EC%9D%98%EC%97%B0%EA%B8%88%EC%88%A04'))
         for page in self.pages:
-            soup = htmlparser.HtmlParser(page).get_soup()
             tmp_dict = {}
-            tmp_dict['id'] = soup.find('div', class_='Profile').find('span', class_='Name').get_text()
-            tmp_dict['link'] = parse.urljoin(base_url, 'userName=' + tmp_dict['id'])
-            tmp_dict['rank'] = soup.find('div', class_='Rank').find('a').get_text()
-            link = soup.find('div', class_='Face').find('img').get('src')
-            print('img link:', link)
-            request.urlretrieve(link, self.img_path + tmp_dict['id'] + '.png')
-            tmp_dict['tier'] = soup.find('div', class_='TierRankInfo').find('span', class_='tierRank').get_text()
-            tmp_dict['lp'] = soup.find('div', class_='TierRankInfo').find('span', class_='LeaguePoints').get_text().split(' ')[0].replace(',', '')
-            tmp_dict['total_win'] = soup.find('div', class_='TierRankInfo').find('span', class_='wins').get_text().replace('W', '')
-            tmp_dict['total_lose'] = soup.find('div', class_='TierRankInfo').find('span', class_='losses').get_text().replace('L', '')
-            tmp_dict['total_win_ratio'] = soup.find('div', class_='TierRankInfo').find('span', class_='winratio').get_text().split(' ')[2]
-            tmp_dict['mmr'] = soup.find('div', id='ExtraView').find('td', class_='MMR').get_text().replace(',', '')
-            tmp_dict['20win'] = soup.find('div', class_='Box').find('div', class_='WinRatioTitle').get_text().split('')[1].replace('W', '')
-            tmp_dict['20lose'] = soup.find('div', class_='Box').find('div', class_='WinRatioTitle').get_text().split('')[2].replace('L', '')
-            tmp_dict['20winratio'] = soup.find('div', class_='Box').find('div', class_='WinRatioText').get_text()
-            tmp_dict['20kill'] = soup.find('div', class_='KDA').find('span', class_='Kill').get_text()
-            tmp_dict['20death'] = soup.find('div', class_='KDA').find('span', class_='Death').get_text()
-            tmp_dict['20assist'] = soup.find('div', class_='KDA').find('span', class_='Assist').get_text()
-            tmp_dict['20kda'] = soup.find('div', class_='KDARatio').find('span', class_='KDARatio').get_text().split(':')[0]
-            tmp_dict['20kda'] = soup.find('div', class_='KDARatio').find('span', class_='CKRate').get_text().split(' ')[2].replace(')', '')
-            self.gameid_data.append(tmp_dict)
+            soup = htmlparser.HtmlParser(page).get_soup()
+            try:
+                tmp_dict['id'] = soup.find('div', class_='Profile').find_all('span', class_='Name')[-1].get_text()
+                print('id:', tmp_dict['id'])
+                tmp_dict['link'] = parse.urljoin(base_url, 'userName=' + tmp_dict['id'])
+                print('link:', tmp_dict['link'])
+                tmp_dict['rank'] = soup.find('div', class_='Rank').find('a').find('span').get_text()
+                link = 'http:' + soup.find('div', class_='Face').find('img').get('src')
+                print('img link:', link)
+                if not os.path.exists(self.img_path):
+                    os.makedirs(self.img_path)
+                request.urlretrieve(link, self.img_path + tmp_dict['id'] + '.png')
+                tmp_dict['tier'] = soup.find('div', class_='TierRankInfo').find('span', class_='tierRank').get_text()
+                tmp_dict['lp'] = soup.find('div', class_='TierRankInfo').find('span', class_='LeaguePoints').get_text().split()[0].replace(',', '')
+                tmp_dict['total_win'] = soup.find('div', class_='TierRankInfo').find('span', class_='wins').get_text().replace('W', '')
+                tmp_dict['total_lose'] = soup.find('div', class_='TierRankInfo').find('span', class_='losses').get_text().replace('L', '')
+                tmp_dict['total_win_ratio'] = soup.find('div', class_='TierRankInfo').find('span', class_='winratio').get_text().split()[2].replace('%', '')
+                tmp_dict['mmr'] = soup.find('div', id='ExtraView').find('td', class_='MMR').get_text().replace(',', '').strip()
+                tmp_dict['20win'] = soup.find('div', class_='GameAverageStats').find('div', class_='WinRatioTitle').get_text().split()[1].replace('W', '')
+                tmp_dict['20lose'] = soup.find('div', class_='GameAverageStats').find('div', class_='WinRatioTitle').get_text().split()[2].replace('L', '')
+                tmp_dict['20winratio'] = soup.find('div', class_='GameAverageStats').find('div', class_='WinRatioText').get_text().replace('%', '').replace('%', '')
+                tmp_dict['20kill'] = soup.find('div', class_='GameAverageStats').find('span', class_='Kill').get_text()
+                tmp_dict['20death'] = soup.find('div', class_='GameAverageStats').find('span', class_='Death').get_text()
+                tmp_dict['20assist'] = soup.find('div', class_='GameAverageStats').find('span', class_='Assist').get_text()
+                tmp_dict['20kda'] = soup.find('div', class_='KDARatio').find('span', class_='KDARatio').get_text().split(':')[0]
+                tmp_dict['20CK'] = soup.find('div', class_='KDARatio').find('span', class_='CKRate').get_text().split()[2].replace(')', '').replace('%', '')
+                self.gameid_data.append(tmp_dict)
+                tmp_dict_2 = {}
+                if soup.find('div', class_='Information').find('div', class_='Team') is not None:
+                    tmp_dict_2['team'] = soup.find('div', class_='Information').find('div', class_='Team').get_text().strip().split('\n')[0]
+                    tmp_dict_2['name'] = soup.find('div', class_='Information').find('span', class_='Name').get_text().replace('[', '').replace(']', '')
+                    tmp_dict_2['id'] = tmp_dict['id']
+                    self.id_mapping.append(tmp_dict_2)
+            except Exception:
+                continue
+        return self.gameid_data, self.id_mapping
 
     def close(self):
         self.browser.close()
